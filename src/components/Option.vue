a3b55b11c2aa6b03c3d1d9920f786ddf82594a26
Jaime Herrera Muñoz <jaimei.herrera@outlook.com>
2021-02-18 11:18:44 -0300

    Sidebar
    Se crea sidebar sin funcionalidad

---
+++ src/components/Option.vue
@@ -0,0 +1,22 @@
+<template>    
+    <v-list-item link>
+        <v-list-item-icon>
+        <v-icon>{{IconName}}</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>{{OptionName}}</v-list-item-title>
+    </v-list-item>
+</template>
+
+<script>
+export default {
+  name: 'Option',
+  props: {
+    IconName: String,
+    OptionName: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="">
+        <v-navigation-drawer permanent expand-on-hover>
+            <v-list>
+                <v-list-item class="px-2">
+                    <v-list-item-avatar>
+                    <v-img src="https://randomuser.me/api/portraits/women/85.jpg"></v-img>
+                    </v-list-item-avatar>
+                </v-list-item>
+
+                <v-list-item link>
+                    <v-list-item-content>
+                    <v-list-item-title class="title">
+                        Sandra Adams
+                    </v-list-item-title>
+                    <v-list-item-subtitle>sandra_a88@gmail.com</v-list-item-subtitle>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+
+            <v-divider></v-divider>
+
+            <v-list nav dense>
+                <Option IconName="mdi-folder" OptionName="My Files"/>
+                <Option IconName="mdi-account-multiple" OptionName="Shared with me"/>
+                <Option IconName="mdi-star" OptionName="Starred"/>
+            </v-list>
+
+            <v-divider></v-divider>
+        </v-navigation-drawer>
+    </div>
+</template>
+
+<script>
+import Option from '../components/Option.vue'
+
+export default {
+    name: "Sidebar",
+    components: {
+        Option
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
