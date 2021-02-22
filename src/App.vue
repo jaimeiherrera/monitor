eb71e3af569b8bd306dfafdc53f7b07fefc2d719
Jaime Herrera Muñoz <jaimei.herrera@outlook.com>
2021-02-22 11:59:01 -0300

    Se integra tabla

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,28 @@
 <template>
   <v-app>
+    <Sidebar />
+    <v-app-bar app>
+      <!-- -->
+    </v-app-bar>
     <v-main>
-      <Sidebar/>
+      <Table />
     </v-main>
+    <v-footer app>
+      <!-- -->
+    </v-footer>
   </v-app>
 </template>
 
 <script>
-import Sidebar from './components/Sidebar';
+import Sidebar from "./components/Sidebar";
+import Table from "./components/Table";
 
 export default {
-  name: 'App',
-
+  name: "App",
   components: {
     Sidebar,
+    Table,
   },
-
   data: () => ({
     //
   }),
--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,47 +1,51 @@
 <template>
-    <div class="">
-        <v-navigation-drawer permanent expand-on-hover>
-            <v-list>
-                <v-list-item class="px-2">
-                    <v-list-item-avatar>
-                    <v-img src="https://randomuser.me/api/portraits/women/85.jpg"></v-img>
-                    </v-list-item-avatar>
-                </v-list-item>
+  <v-navigation-drawer app permanent expand-on-hover>
+    <v-list>
+      <v-list-item class="px-2">
+        <v-list-item-avatar>
+          <v-img src="https://randomuser.me/api/portraits/women/85.jpg"></v-img>
+        </v-list-item-avatar>
+      </v-list-item>
 
-                <v-list-item link>
-                    <v-list-item-content>
-                    <v-list-item-title class="title">
-                        Sandra Adams
-                    </v-list-item-title>
-                    <v-list-item-subtitle>sandra_a88@gmail.com</v-list-item-subtitle>
-                    </v-list-item-content>
-                </v-list-item>
-            </v-list>
+      <v-list-item link>
+        <v-list-item-content>
+          <v-list-item-title class="title"> Sandra Adams </v-list-item-title>
+          <v-list-item-subtitle>sandra_a88@gmail.com</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
 
-            <v-divider></v-divider>
+    <v-divider></v-divider>
 
-            <v-list nav dense>
-                <Option IconName="mdi-folder" OptionName="My Files"/>
-                <Option IconName="mdi-account-multiple" OptionName="Shared with me"/>
-                <Option IconName="mdi-star" OptionName="Starred"/>
-            </v-list>
+    <v-list nav dense>
+      <div v-for="opt in OptionMenu" :key="opt.Opt">
+        <Option :IconName="opt.Icon" :OptionName="opt.Opt" />
+      </div>
+    </v-list>
 
-            <v-divider></v-divider>
-        </v-navigation-drawer>
-    </div>
+    <v-divider></v-divider>
+  </v-navigation-drawer>
 </template>
 
 <script>
-import Option from '../components/Option.vue'
+import Option from "../components/Option.vue";
 
 export default {
-    name: "Sidebar",
-    components: {
-        Option
-    }
-}
+  name: "Sidebar",
+  components: {
+    Option,
+  },
+  data: () => {
+    return {
+      OptionMenu: [
+        { Icon: "mdi-folder", Opt: "My Files" },
+        { Icon: "mdi-account-multiple", Opt: "Shared with me" },
+        { Icon: "mdi-star", Opt: "Starred" },
+      ],
+    };
+  },
+};
 </script>
 
 <style scoped>
-
 </style>
