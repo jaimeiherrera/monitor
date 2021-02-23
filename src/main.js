55d48ae11bb766a900b0ca63c202af4590ed86e2
Jaime Herrera Muñoz <jaimei.herrera@outlook.com>
2021-02-23 19:18:01 -0300

    Se corrigen errores que impedian compilar

--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
-import express from './plugins/express';
 
 Vue.config.productionTip = false
 
 new Vue({
   vuetify,
-  express,
   render: h => h(App)
 }).$mount('#app')
